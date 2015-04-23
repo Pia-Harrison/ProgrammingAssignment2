85081bb50f9051e4a54083f636f91b76179d2d5a
Pia Harrison <pia.harrison12@gmail.com>
2015-04-23 21:50:17 +1000

    finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## a pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## the function makeCacheMatrix stores four functions:
+## 1.set() function: changes the matrix stored in the main function to the y input from the main function & m sets any 
+## previously saved matrix to NULL so that the inverse matrix can be recalculated from the y input provided
+## 2.get() function: returns the matrix stored in the main function
+## 3.setinverse() function: stores the matrix in a variable m into the main function makeCacheMatrix
+## 4.getinverse() function: stores teh matrix in a variable m into the main function and returns it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function()x
+  setinv <- function(solve) m <<- solve
+  getinv <- function()m
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## the cacheSolve function uses the functions in makeCacheMatrix to checks 
+## if there is an inverse matrix stored in the 'cache'; otherwise,
+## it calculates the inverse of the matrix
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  m <- x$getinv()
+  if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+  }
+  data <- x$get()          ## gets the matrix saved in the main function as x
+  m <- solve(data, ...)    ## m calculates the inverse matrix here!
+  x$setinv(m)              ## stores the inverse matrix in the object created in makeCacheMatrix
 }
+
+m <- matrix(c(-1,-2,1,1), 2,2)
+x <- makeCacheMatrix(m)
+x$get()
+
+
+inv <- cacheSolve(x)
+inv
+
+inv <- cacheSolve(x)
